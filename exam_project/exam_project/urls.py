a6544027eb2ebc79d3c0a252d7e5dd025ce3853f
Алексей <aiex.0115970@gmail.com>
2022-02-26 11:28:48 +0500

    StartProject

---
+++ exam_project/exam_project/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('', include('mainapp.urls', namespace='mainapp')),
+
+    path('auth/', include('authapp.urls', namespace='authapp')),
+
+    path('basket/', include('basketapp.urls', namespace='basketapp')),
+
+    path('admin/', admin.site.urls),
+]
---
+++ exam_project/mainapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+app_name = 'mainapp'
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+]
