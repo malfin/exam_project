d2d0ce4e3c7c11534523f34724ae1eaab934fa17
Алексей <aiex.0115970@gmail.com>
2022-03-27 16:43:30 +0500

    Add models:       - Category       - Products       - Basket

--- exam/basketapp/models.py
+++ exam/basketapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from authapp.models import KpkUser
+from mainapp.models import Products
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(KpkUser, on_delete=models.CASCADE, verbose_name='пользователь')
+    products = models.ForeignKey(Products, on_delete=models.CASCADE, verbose_name='товар')
+    created_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.user.login
+
+    class Meta:
+        verbose_name = 'Корзина'
+        verbose_name_plural = 'Корзин'
--- exam/mainapp/models.py
+++ exam/mainapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(verbose_name='Название категории', max_length=128)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Products(models.Model):
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
+    name = models.CharField(verbose_name='Название товара', max_length=128)
+    price = models.DecimalField(verbose_name='Цена', max_digits=10, decimal_places=2)
+    image = models.ImageField(verbose_name='изображение', upload_to=f'products/{name}/')
+    created_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
